22d7203ab8b78d80d53c216c90cbf5fe6c91627b
tanimsk <sktanim5800@gmail.com>
2025-03-01 11:04:04 +0600

    added cstdint header

--- XT_DAC_Audio.h
+++ XT_DAC_Audio.h
@@ -18,7 +18,7 @@
 
 #pragma once
 #include "MusicDefinitions.h"
-
+#include <cstdint>
 #define BUFFER_SIZE_DEFAULT 4000 			    // Size of buffer to store data to send to DAC. 3 bytes minimum!
 												// 4000 bytes (Default) should allow for very slow main loops
 												// that repeat only about every 100Hz, If your main loop is much faster
